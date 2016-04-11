d42b0f2d67fe5914e67cb70d844b6738d1142928
Faebeee <faebeee@gmail.com>
2016-04-11 12:51:29 +0200

    init

---
+++ src/Component/PixiSpriteComponent.js
@@ -0,0 +1,22 @@
+class PixiSpriteComponent extends cxComponent
+{
+    constructor( texture, size, position ){
+        super();
+        this.tag = 'cx.pixi.component.sprite';
+
+        size = size || { x: 1, y: 1};
+        position = position || { x: 0, y: 0};
+
+        this.texture = texture;
+        this.sprite = new PIXI.Sprite(texture);
+
+        this.sprite.anchor.x = 0.5;
+        this.sprite.anchor.y = 0.5;
+
+        this.sprite.position.x = position.x;
+        this.sprite.position.y = position.y;
+
+        this.sprite.scale.x = size.x;
+        this.sprite.scale.y = size.y;
+    }
+}
---
+++ src/System/PixiSystem.js
@@ -0,0 +1,39 @@
+class PixiSystem extends cxVoidSystem
+{
+    constructor(container, width, height)
+    {
+        super();
+        this.tag = 'cx.pixi.system';
+
+        this.width = width;
+        this.height = height;
+        this.container = container || document.body;
+
+        this.stage = new PIXI.Stage(0x66FF99);
+        this.renderer = PIXI.autoDetectRenderer(width, height);
+
+
+        this.container.appendChild(this.renderer.view);
+    }
+
+    added(cxEntity)
+    {
+        if(cxEntity.hasComponent('cx.pixi.component.sprite')){
+            let comp = cxEntity.getComponent('cx.pixi.component.sprite');
+            this.stage.addChild(comp.sprite);
+        }
+    }
+
+    removed(cxEntity)
+    {
+        if(cxEntity.hasComponent('cx.pixi.component.sprite')){
+            let comp = cxEntity.getComponent('cx.pixi.component.sprite');
+            //this.stage.addChild(comp.sprite);
+        }
+    }
+
+    update ()
+    {
+         this.renderer.render(this.stage);
+    }
+}
