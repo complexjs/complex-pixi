825698f53d723e4b5b6644c830eb46048952f5d4
Faebeee <faebeee@gmail.com>
2016-05-26 16:18:25 +0200

    implement remove

--- src/System/PixiSystem.js
+++ src/System/PixiSystem.js
@@ -5,22 +5,27 @@ var PIXI = require('pixi.js');
 
 class PixiSystem extends cxVoidSystem
 {
-    constructor(container, width, height)
+    constructor(container, width, height, color)
     {
         super();
+        color = color || 0x333333;
         this.tag = 'cx.pixi.system';
 
         this.width = width;
         this.height = height;
         this.container = container || document.body;
 
-        this.stage = new PIXI.Stage(0x66FF99);
+        this.stage = new PIXI.Stage(color);
         this.renderer = PIXI.autoDetectRenderer(width, height);
-
 
         this.container.appendChild(this.renderer.view);
     }
 
+    /**
+     * [added description]
+     * @param  {[type]} cxEntity [description]
+     * @return {[type]}          [description]
+     */
     added(cxEntity)
     {
         if(cxEntity.hasComponent('cx.pixi.component.sprite')){
@@ -29,14 +34,23 @@ class PixiSystem extends cxVoidSystem
         }
     }
 
+    /**
+     * [removed description]
+     * @param  {[type]} cxEntity [description]
+     * @return {[type]}          [description]
+     */
     removed(cxEntity)
     {
         if(cxEntity.hasComponent('cx.pixi.component.sprite')){
             let comp = cxEntity.getComponent('cx.pixi.component.sprite');
-            //this.stage.addChild(comp.sprite);
+            this.stage.removeChild(comp.sprite);
         }
     }
 
+    /**
+     * [update description]
+     * @return {[type]} [description]
+     */
     update ()
     {
          this.renderer.render(this.stage);
