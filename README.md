a2b55c1deac3b0941fd22e44ea33bdfbad664ccb
faebeee <faebeee@gmail.com>
2016-11-04 17:54:29 +0100

    upgrade pixi

---
+++ README.md
@@ -0,0 +1,13 @@
+# complex-pixi
+
+## Install
+
+    npm i complex-pixi --save
+
+## Usage
+
+### System
+
+Load the system in your Scene
+
+    let PixiSystem = require('complex-pixi').cxPixiSystem;
--- module.js
+++ module.js
@@ -4,6 +4,5 @@ module.exports = {
     cxPixiSpriteComponent : require('./src/Component/cxPixiSpriteComponent'),
     cxPixiComponent : require('./src/Component/cxPixiComponent'),
     cxPixiSystem : require('./src/System/cxPixiSystem'),
-    cxPixiSpriteComponent : require('./src/Component/cxPixiSpriteComponent'),
     cxPixiSpriteComponentParser : require('./src/Parser/cxPixiSpriteComponentParser'),
 };
