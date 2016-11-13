3ec118f2a3e6fa82074ccf50aa3ad0b28087708f
faebeee <faebeee@gmail.com>
2016-11-13 19:33:38 +0100

    update dep

--- module.js
+++ module.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-    cxPixiSpriteComponent : require('./src/Component/cxPixiSpriteComponent'),
     cxPixiComponent : require('./src/Component/cxPixiComponent'),
     cxPixiSystem : require('./src/System/cxPixiSystem'),
     cxPixiSpriteComponentParser : require('./src/Parser/cxPixiSpriteComponentParser'),
